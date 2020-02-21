9d4bef148366708577dcbb8a8ed53d8c261e1e64
cerebrium <nicholas.m.shankland@gmail.com>
2020-02-21 15:44:21 -0800

    initial commit

--- mapAttempt/App.js
+++ mapAttempt/App.js
@@ -1,29 +1,143 @@
-import React from 'react';
-import {Text, StyleSheet} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import React, { useState, useEffect} from 'react'
+import { StyleSheet, View, Text, TextInput, ImageBackground, Button } from 'react-native'
+import titleBackground from './cobro-titleScreen.jpg'
+import { NativeRouter as Router, Route, Link } from "react-router-native"
+import MapPage from './routes/map'
 
 const App = () => {
+  // state
+  const [ userName, setUserName ] = useState('')
+  const [ userEmail, setUserEmail ] = useState('')
+  const [ user, setUser ] = useState(null)
+
+  // function to send to backend
+  const susbmitInfo = (e) => {
+    console.log('state: ' + userEmail + ' other comment')
+    fetch('http://10.1.7.200:3001/auth/signup', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: userName,
+        email: userEmail,
+      }),
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      setUser(responseJson)
+      console.log(responseJson)
+    })
+  }
+
+  // conditional rendering for the login bit
+  var userNameGotten
+  if (user) {
+    userNameGotten = (
+      <Router>
+        <Route exact path='/' component={MapPage}/>
+      </Router>
+    )
+  } else {
+    userNameGotten = (
+      <ImageBackground
+        source={titleBackground}
+        style={{
+          height: '100%',
+          width: '100%'
+        }}
+      >
+        <View style={styles.container}>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>Potter Go</Text>
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.inputLabel}>Username:</Text>
+            <TextInput
+              style={styles.inputBoxes}
+              placeholder="NShankland"
+              onChangeText={(text) => setUserName(text)}
+              userName={userName}
+            />
+            <Text style={styles.inputLabel}>Email:</Text>
+            <TextInput
+              style={styles.inputBoxes}
+              placeholder="Nshankland@gmail.com"
+              onChangeText={(text) => setUserEmail(text)}
+              userEmail={userEmail}
+            />
+          </View>
+          <View style={styles.submitButton}>
+            <Button
+              title='Submit'
+              color='gold'
+              onPress={susbmitInfo}
+            ></Button>
+          </View>
+        </View>
+      </ImageBackground>
+    )
+  }
+
   return (
     <>
-      <Text>Hello World</Text>
-      <MapView
-       provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-       style={styles.map}
-       region={{
-         latitude: 37.78825,
-         longitude: -122.4324,
-         latitudeDelta: 0.015,
-         longitudeDelta: 0.0121,
-       }}
-     >
-     </MapView>
+      {userNameGotten}
     </>
   );
 };
 
+// Styles
 const styles = StyleSheet.create({
-  map: {
+  container: {
     flex: 1,
+    alignItems: 'center'
   },
+  title: {
+    fontSize: 60,
+    color: 'gold'
+  },
+  inputBoxes: {
+    height: 30, 
+    borderColor: 'gray', 
+    width: 200,
+    backgroundColor: 'lightgrey',
+    borderRadius: 15,
+    textAlign: 'center'
+  },
+  inputContainer: {
+    marginTop: 200,
+    height: '25%',
+    width: '80%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#060E18',
+    borderRadius: 15,
+    opacity: .9
+  },
+  inputLabel: {
+    fontSize: 40,
+    marginTop: 10,
+    color: 'gold'
+  },
+  titleContainer: {
+    marginTop: 45,
+    height: '10%',
+    width: '80%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#060E18',
+    borderRadius: 15,
+    opacity: .9
+  },
+  submitButton: {
+    marginTop: 15,
+    backgroundColor: '#060E18',
+    opacity: .9,
+    height: 40,
+    width: 100,
+    borderRadius: 15
+  }
 });
+
 export default App;
---
+++ mapAttempt/routes/map.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Text, StyleSheet} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+
+const App = () => {
+  return (
+    <>
+      <MapView
+       provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+       style={styles.map}
+       region={{
+         latitude: 37.78825,
+         longitude: -122.4324,
+         latitudeDelta: 0.015,
+         longitudeDelta: 0.0121,
+       }}
+     >
+     </MapView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+});
+export default App;
